f47d9512c9219e019e54a8fb9c96c8ded17a8743
JacksonBelloli <jakson.belloli@gmail.com>
2018-11-28 16:06:33 -0200

    Terminando javascript

--- CellOn-colorlib/CellOn-colorlib/js/controller.js
+++ CellOn-colorlib/CellOn-colorlib/js/controller.js
@@ -1,12 +1,11 @@
-$(document).ready(function() {
+function EncontraFilm (film1, film2) {
     $.ajax({
         datatype: 'json',
         type: 'GET',
         contentType: "application/json; charset=utf-8",
-        data: "{}",
         url: "http://rest-service.guides.spring.io/greeting"
     }).then(function(data) {
        $('#get-id').append(data.id);
        $('#get-content').append(data.content);
     });
-});
+}
--- CellOn-colorlib/CellOn-colorlib/js/custom.js
+++ CellOn-colorlib/CellOn-colorlib/js/custom.js
@@ -1,37 +1,34 @@
 $(document).ready(function() {
 
     $('img').click(function() {
-         film1 = document.getElementById('remove-film1');
-         film2 = document.getElementById('remove-film2');
          $('img.active').removeClass("active"); 
          $(this).addClass("active"); 
      });
      $('#btnEscolher-1').click(function(){
-        var selecionada = $("img.active").attr("src");
-        document.getElementById("film-1").innerHTML = '<img class="new-1">';
+        var selected = $("img.active").attr("src"),
+            name = $('img.active').attr('data-name');
+        document.getElementById("film-1").innerHTML = '<img data-name="'+ name +'" class="new-1" style="width: 200px;" src="'+ selected + '">';
         $('div#film-1').removeClass('thumb-film');
-        $("img.new-1").css("width", "200px");
-        $("img.new-1").attr("src", selecionada);
         $("img.active").removeClass("active");
     });
     $('#btnEscolher-2').click(function(){
-        var selecionada = $("img.active").attr("src");
-        document.getElementById("film-2").innerHTML = '<img class="new-2">';
+        var selected = $("img.active").attr("src"),
+            name = $('img.active').attr('data-name');
+        document.getElementById("film-2").innerHTML = '<img data-name="'+ name +'" class="new-2" style="width: 200px;" src="'+ selected +'">';
         $('div#film-2').removeClass('thumb-film');
-        $("img.new-2").css("width", "200px");
-        $("img.new-2").attr("src", selecionada);
         $("img.active").removeClass("active");
     });
     $('#comb').on('click', function(){
         if (!$('#comb').attr('id').includes('back')){
-            var selecionada = $('#film-1 img').attr('src');
-            $('#film-1').css('display', 'none');
-            $('#film-2').css('display', 'none');
-            $('<div id="result" class="col-md-12"><div class="link-modal"><img class="tam" src="' + selecionada + '"> </div></div>').insertAfter('div.single-contact');
+            var film1 = $('#film-1 img').attr('data-name'),
+                film2 = $('#film-2 img').attr('data-name'),
+                new_film = EncontraFilm (film1, film2) ;
+
+            $('<div id="result" class="col-md-12"><div class="link-modal"><img class="tam" src="filmes/' + new_film + '.jpg"> </div></div>').insertAfter('div.single-contact');
             $('img.tam').css('width', '200px');
             $('.btn-comb').html('<a class="genric-btn primary-border circle arrow" onclick="voltar();">Voltar<span class="lnr lnr-arrow-right"></span></a>');            
-            film1.remove();
-            film2.remove();
+            $('remove-fim1').remove();
+            $('remove-film2').remove();
         }
     });
  });
